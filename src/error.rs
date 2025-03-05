b5febf255d28748deb30ea42b88dbca89b310105
Sam Kenney <sam.kenney@me.com>
2025-03-05 21:26:48 +1100

    chore: update error enum

--- src/error.rs
+++ src/error.rs
@@ -34,30 +34,46 @@ pub enum Error {
     InvalidVar(String),
 }
 
+impl std::error::Error for Error {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            Self::Io(e) => Some(e),
+            Self::InvalidConfig(e) => Some(e),
+            Self::Http(e) => Some(e),
+            Self::UnknownHttpHeader(e) => Some(e),
+            Self::InvalidHttpHeader(e) => Some(e),
+            Self::Regex(e) => Some(e),
+            Self::NotifyRust(e) => Some(e),
+            _ => None,
+        }
+    }
+}
+
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let message = match self {
-            Self::NoConfig => {
-                "No config file found, please create one or provide the path with --config".into()
-            }
+            Self::NoConfig => "no config file found".into(),
             Self::ConfigConflict { path } => {
-                format!("Config file `{}` already exists", path.to_string_lossy())
+                format!("config file `{}` already exists", path.display())
             }
-            Self::InvalidConfig(e) => format!("Invalid config file: {e}"),
-            Self::Http(e) => format!("An error occurred when sending a request: {e}"),
+
+            Self::InvalidConfig(e) => format!("invalid config file: {e}"),
+            Self::Http(e) => format!("an error occurred when sending a request: {e}"),
             Self::UnknownHttpHeader(e) => format!("{e}"),
             Self::InvalidHttpHeader(e) => format!("{e}"),
-            Self::Regex(e) => format!("Failed to parse regex: {e}"),
-            Self::Io(e) => format!("IO: {e}"),
+            Self::Regex(e) => format!("failed to parse regex: {e}"),
+            Self::Io(e) => format!("io: {e}"),
             Error::NoMessage => {
-                "A message must be provided when not streaming notifications".into()
+                "a message must be provided when not streaming notifications".into()
             }
-            Error::StreamAndMessage => "A message cannot be provided when using streaming".into(),
-            Error::NotifyRust(e) => format!("Failed to send desktop notification: {e}"),
+            Error::StreamAndMessage => "a message cannot be provided when using streaming".into(),
+            Error::NotifyRust(e) => format!("failed to send desktop notification: {e}"),
             Error::VarNotSet(var) => {
-                format!("The env var {var} was specified in noti.yaml but has not been set")
+                format!("env var {var} was specified in noti.yaml but has not been set")
             }
-            Error::InvalidVar(var) => format!("The env var {var} is not UTF-8 and cannot be parsed"),
+            Error::InvalidVar(var) => {
+                format!("env var {var} is not UTF-8 and cannot be parsed")
+            }
         };
 
         write!(f, "{message}")
